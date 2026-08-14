6437ffddad88d13e10a8014e51914f8db6b738aa
manit2004 <manit.roy@research.iiit.ac.in>
2026-08-14 19:28:34 +0530

    feat(sgemmtr): config files added for sgemmtr

--- repl.mjs
+++ repl.mjs
@@ -2,7 +2,7 @@ import repl from "node:repl";
 import {
   init, cleanup,
   GpuVector, GpuMatrix,
-  sscal, sswap, saxpy, scopy, sdot, sasum, dasum, snrm2, isamax, idamax, srot, srotm, sgemv, ssymv, strsv, sger, ssyr, ssyr2, sgemm,
+  sscal, sswap, saxpy, scopy, sdot, sasum, dasum, snrm2, isamax, idamax, srot, srotm, sgemv, ssymv, strsv, sger, ssyr, ssyr2, sgemm, sgemmtr,
 } from "wgblas";
 
 const device = await init();
@@ -13,7 +13,7 @@ Object.assign(r.context, {
   device,
   GpuVector, GpuMatrix,
   Float32Array, Float64Array,
-  sscal, sswap, saxpy, scopy, sdot, sasum, dasum, snrm2, isamax, idamax, srot, srotm, sgemv, ssymv, strsv, sger, ssyr, ssyr2, sgemm,
+  sscal, sswap, saxpy, scopy, sdot, sasum, dasum, snrm2, isamax, idamax, srot, srotm, sgemv, ssymv, strsv, sger, ssyr, ssyr2, sgemm, sgemmtr,
 });
 
 r.defineCommand("help", {
@@ -40,6 +40,7 @@ r.defineCommand("help", {
   ssyr  (device, uplo, n, alpha, x, incx, A, lda)
   ssyr2 (device, uplo, n, alpha, x, incx, y, incy, A, lda)
   sgemm (device, transA, transB, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
+  sgemmtr(device, uplo, transA, transB, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
 
   GpuVector.from(Float32Array)   v.read()  v.destroy()
   GpuVector.from(Float64Array)   v.read()  v.destroy()  (for dasum)
