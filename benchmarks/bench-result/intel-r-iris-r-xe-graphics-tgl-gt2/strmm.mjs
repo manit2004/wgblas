6a5e10cc59468327a79547fee50cf60ff8d83994
manit2004 <manit.roy@research.iiit.ac.in>
2026-08-17 21:07:49 +0530

    feat(strmm): bench and docs added

---
+++ benchmarks/bench-result/intel-r-iris-r-xe-graphics-tgl-gt2/strmm.mjs
@@ -0,0 +1,24 @@
+/**
+ * Benchmark results for strmm on Intel R Iris R Xe Graphics Tgl Gt2.
+ *
+ * ## Intel R Iris R Xe Graphics Tgl Gt2
+ *
+ * | n | compute ms | GB/s |
+ * |---|------------|------|
+ * | 32 | 0.0655 | 0.5000 |
+ * | 64 | 0.2621 | 0.5000 |
+ * | 128 | 0.5243 | 1.0000 |
+ * | 256 | 0.6554 | 3.2000 |
+ * | 512 | 20.7094 | 0.4051 |
+ * | 1024 | 222.0360 | 0.1511 |
+ *
+ * ![strmm-default GB/s chart](../../../assets/benchmarks/intel-r-iris-r-xe-graphics-tgl-gt2/strmm/gbps-default.svg)
+ *
+ * ![strmm-default ms chart](../../../assets/benchmarks/intel-r-iris-r-xe-graphics-tgl-gt2/strmm/ms-default.svg)
+ *
+ * ## See also
+ *
+ * - [strmm.js](https://github.com/manit2004/wgblas/blob/main/benchmarks/strmm/wgblas/strmm.js) — WebGPU benchmark script
+ *
+ * @module benchmarks/intel-r-iris-r-xe-graphics-tgl-gt2/strmm
+ */
